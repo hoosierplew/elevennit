eb0f533feff3f55cc165d1f291b59ae1d98a39bb
Shawn Plew <shawn.plew@mobiwm.com>
2014-11-11 14:16:31 -0500

    update flash message styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_class(name)
+    alert_type = case name.to_s
+      when 'notice'  then 'success'
+      when 'error'   then 'alert'
+      when 'alert'   then 'alert'
+      when 'info'    then 'info'
+      when 'warning' then 'warning'
+      else 'secondary'
+    end
+    alert_type
+  end
 end
