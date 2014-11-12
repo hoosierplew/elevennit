69c4be2d743fbdb6eadc611e00939fdbbd81a78d
Shawn Plew <shawn.plew@mobiwm.com>
2014-11-11 20:17:51 -0500

    create category DB stuff

---
+++ app/models/concerns/category.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+  validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :description, length: { minimum: 2, maximum: 500 }
+  validates :sidebar, length: { maximum: 5120 }
+  validates :submission_text, length: { maximum: 5120 }
+end
