9162164c665db9479b5ba87dde61646bbfb056ce
Shawn Plew <shawn.plew@mobiwm.com>
2014-11-12 14:10:26 -0500

    change the default scope for posts and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveRecord::Base
-  has_many :posts
   validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 20 }
   validates :title, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
   validates :sidebar, length: { maximum: 5120 }
   validates :submission_text, length: { maximum: 1024 }
+
+  has_many :posts
+  default_scope { order(:name) }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
-  belongs_to :category
   validates :title, length: { minimum: 2, maximum: 255 }, presence: true
   validates :link, presence: true, if: :link?
   validates :link, length: { maximum: 2000 }
   validates :body, presence: true, if: :text?
+
   enum post_type: [:link, :text]
+
+  belongs_to :category
+  default_scope { order('updated_at DESC') }
 end
