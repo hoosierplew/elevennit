ace5a8b7841315c12c565637fe7f9fcaecb174a6
Shawn Plew <shawn.plew@mobiwm.com>
2014-11-13 12:03:45 -0500

    nestable comments form build out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,16 @@ class CommentsController < ApplicationController
     redirect_to post
   end
 
+  def new
+    @parent = Comment.find params[:parent_id]
+    post = @parent.commentable
+    @comment = Comment.build_from post, current_user.id, ''
+    @comment.parent_id = @parent.id
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
 
   private ##########################################################################################
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts
-  resources :comments, only: :create
+  resources :comments, only: [:create, :new]
   resources :categories
 
 
